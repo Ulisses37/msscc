4064a2e1e927e2ba97a1622759c9a7dfa7ece3e7
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2026-04-04 12:29:16 -0700

    feat(SCRUM-223): add columns to admin memberships table

--- frontend/app/(admin)/dashboard/memberships/page.tsx
+++ frontend/app/(admin)/dashboard/memberships/page.tsx
@@ -8,8 +8,26 @@ export default function AdminMembershipsPage() {
       </header>
 
       <main>
-        <table>
+        <table className="overflow-x-auto rounded border border-gray-300">
+          <tbody className="min-w-full">
+            <tr className="border-b border-gray-200">
+              <td className="px-4 py-2">col 1</td>
+              <td className="px-4 py-2">col 2</td>
+              <td className="px-4 py-2">col 3</td>
+              <td className="px-4 py-2">col 4</td>
+              <td className="px-4 py-2">col 5</td>
+              <td className="px-4 py-2">col 6</td>
+            </tr>
 
+            <tr className="border-b border-gray-200">
+              <td className="px-4 py-2">col 1</td>
+              <td className="px-4 py-2">col 2</td>
+              <td className="px-4 py-2">col 3</td>
+              <td className="px-4 py-2">col 4</td>
+              <td className="px-4 py-2">col 5</td>
+              <td className="px-4 py-2">col 6</td>
+            </tr>
+          </tbody>
         </table>
       </main>
     </div>
