4f0a4335575a68775341629e9c5ac9b7993dd05c
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2026-04-04 13:53:38 -0700

    fix(SCRUM-226): add spacing to entire page div

--- frontend/app/(admin)/dashboard/memberships/page.tsx
+++ frontend/app/(admin)/dashboard/memberships/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function AdminMembershipsPage() {
   return(
-    <div>
-      <header>
+    <div className="p-6">
+      <header className="mb-6">
         <h1>View Memberships</h1>
       </header>
     </div>
