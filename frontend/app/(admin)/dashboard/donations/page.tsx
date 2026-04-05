ae8b7d1bf84f28b3f554f8af99461be9c3964487
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2026-04-05 14:27:25 -0700

    feat(SCRUM-225): add sample rows to admin donations page

--- frontend/app/(admin)/dashboard/donations/page.tsx
+++ frontend/app/(admin)/dashboard/donations/page.tsx
@@ -9,7 +9,23 @@ export default function AdminDonationsPage() {
 
       <main>
         <table>
+          <tbody>
+            <tr className="border-b border-gray-200">
+              <td className="px-4 py-2">col 1</td>
+              <td className="px-4 py-2">col 2</td>
+              <td className="px-4 py-2">col 3</td>
+              <td className="px-4 py-2">col 4</td>
+              <td className="px-4 py-2">col 5</td>
+            </tr>
 
+            <tr className="border-b border-gray-200">
+              <td className="px-4 py-2">col 1</td>
+              <td className="px-4 py-2">col 2</td>
+              <td className="px-4 py-2">col 3</td>
+              <td className="px-4 py-2">col 4</td>
+              <td className="px-4 py-2">col 5</td>
+            </tr>
+          </tbody>
         </table>
       </main>
     </div>
