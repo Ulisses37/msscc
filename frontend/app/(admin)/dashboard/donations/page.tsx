e7476aa8155b5701a616acaefec08e4acc6152f1
DavidTimothyNam <87089167+DavidTimothyNam@users.noreply.github.com>
2026-04-05 14:59:00 -0700

    feat(SCRUM-236): update styling of each row in admin donations page

--- frontend/app/(admin)/dashboard/donations/page.tsx
+++ frontend/app/(admin)/dashboard/donations/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { sampleDonationData } from "./sampleData";
+import { formatCurrency } from "@/utils/formatCurrency";
 
 export default function AdminDonationsPage() {
   return(
@@ -9,19 +10,37 @@ export default function AdminDonationsPage() {
       </header>
 
       <main>
-        <table>
-          <tbody>
-            {sampleDonationData.map((donation, index) => (
-              <tr key={index} className="border-b border-gray-200">
-                <td className="px-4 py-2">{donation.date}</td>
-                <td className="px-4 py-2">{donation.name}</td>
-                <td className="px-4 py-2">{donation.paymentMethod}</td>
-                <td className="px-4 py-2">{donation.frequency}</td>
-                <td className="px-4 py-2">{donation.amount}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="overflow-x-auto">
+          <div className="inline-block rounded border border-gray-300 bg-gray-50 p-2">
+            <table className="w-auto border-separate border-spacing-y-1">
+              <tbody>
+                {sampleDonationData.map((donation, index) => (
+                  <tr key={index} className="bg-white hover:bg-gray-50">
+                    <td className="px-4 py-3 border-y border-l border-gray-300 rounded-l-md">
+                      {donation.date}
+                    </td>
+
+                    <td className="px-4 py-3 border-y border-gray-300">
+                      {donation.name}
+                    </td>
+
+                    <td className="px-4 py-3 border-y border-gray-300">
+                      {donation.paymentMethod}
+                    </td>
+
+                    <td className="px-4 py-3 border-y border-gray-300 capitalize">
+                      {donation.frequency}
+                    </td>
+
+                    <td className="px-4 py-3 border-y border-r border-gray-300 rounded-r-md">
+                      {formatCurrency(donation.amount)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </main>
     </div>
   );
