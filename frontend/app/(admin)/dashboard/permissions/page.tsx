e58006f614367d5990150f32bbc9eecc08be1b6e
TheColeTanner <coletanner02@gmail.com>
2026-04-09 23:19:02 -0700

    Toggle between executive and normal view

--- frontend/app/(admin)/dashboard/permissions/page.tsx
+++ frontend/app/(admin)/dashboard/permissions/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { SampleAdminRecordData } from "./sampleData";
 import PermissionCard from "./permissionsCard";
+import { useState } from "react";
 
 export default function AdminPermissionPage() {
+  const [isExecutive, setIsExecutive] = useState(false);
+
   return (
     <div className="flex justify-center min-h-screen">
       <div className="border w-[70%] rounded-md p-6 flex flex-col gap-4 bg-zinc-300">
+                <button
+          onClick={() => setIsExecutive(!isExecutive)}
+          className={`${isExecutive ? "bg-green-500" : "bg-gray-500"} text-white font-bold py-2 px-4 rounded`}>
+          {isExecutive ? "Executive View" : "Standard View"}
+        </button>
         <div className="mb-10 flex justify-center align-center">
           <p className="text-6xl font-bold text-black font-serif">Permissions</p>
         </div>
         {SampleAdminRecordData.map((record) => (
-          <PermissionCard key={record.email} permission={record} />
+          <PermissionCard key={record.email} permission={record} isExecutive={isExecutive} />
         ))}
       </div>
     </div>
--- frontend/app/(admin)/dashboard/permissions/permissionsCard.tsx
+++ frontend/app/(admin)/dashboard/permissions/permissionsCard.tsx
@@ -1,13 +1,13 @@
 "use client";
+import { useState } from "react";
 import { AdminRecord } from "./sampleData";
 
 interface PermissionCardProps {
   permission: AdminRecord;
+  isExecutive: boolean;
 }
 
-export default function PermissionCard ({ permission }: PermissionCardProps) {
-  console.log(permission);  // check what's actually being passed
-  console.log(permission.permissionsList);  // check if this is undefined
+export default function PermissionCard ({ permission, isExecutive }: PermissionCardProps) {
   return (
     <div className = "border rounded-md p-6 flex divide-x bg-msscc-white h-32 items-center">
       <div className = "basis-2/12">
@@ -20,8 +20,8 @@ export default function PermissionCard ({ permission }: PermissionCardProps) {
       <div className = "basis-7/12 flex-col gap-2 pl-4 grid grid-cols-3">
       {permission.permissionsList.slice().sort((a, b) => Number(b.hasPermission) - Number(a.hasPermission))
       .map((perm) => (
-        <p key={perm.permissionName} className={perm.hasPermission ? "text-green-600" : "text-gray-400"}>
-        {perm.permissionName}
+        <p key={perm.permissionName}>
+          {PermissionIcon({ permissionName: perm.permissionName, hasPermission: perm.hasPermission, isExecutive })}
         </p>
       ))}
       </div>
@@ -29,3 +29,23 @@ export default function PermissionCard ({ permission }: PermissionCardProps) {
   );
 }
 
+function PermissionIcon({ permissionName, hasPermission, isExecutive }: { permissionName: string, hasPermission: boolean, isExecutive: boolean }) {
+  const [active, setActive] = useState(hasPermission);
+
+  if(isExecutive) {
+    return (
+      <button onClick={() => setActive(!active)}
+        className={`${active ? "bg-green-500 hover:bg-green-700" : "bg-gray-500 hover:bg-gray-700 opacity-50"} text-white font-bold py-2 px-4`}>
+        {permissionName}
+      </button>
+    );
+  }else{
+    return hasPermission
+      ? <span className="text-green-600 font-bold border-2 p-1  border-green-600 rounded-sm">{permissionName}</span>
+      : null;
+  }
+}
+
+<script>
+  const
+</script>
