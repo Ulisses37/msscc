97afeac213bcc72fa4bf1f83832a01c5627f2dd7
lucasok11 <lucasbilyk@csus.edu>
2026-04-12 08:17:59 -0700

    feat(SCRUM-218): add FB icon to banner

--- frontend/components/layout/SocialLink.tsx
+++ frontend/components/layout/SocialLink.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import SmallIcon from '@/public/icons/FB_Icon.svg';
+import Image from 'next/image';
+import fbIcon from '@/public/icons/FB_Icon.svg';
 
-export const LeftBannerIcon = () => {
+export const SocialIcon = () => {
   return (
     <a
       href="https://www.facebook.com/MSSCC1/"
       target="_blank"
       rel="noopener noreferrer"
-      className="absolute left-4 top-1/2 -translate-y-1/2 z-20"
+      className="absolute left-8 top-1/2 -translate-y-1/2 z-20"
     >
-      <SmallIcon className="w-8 h-8 md:w-10 md:h-10 drop-shadow-lg" />
+      <Image 
+        src={fbIcon} 
+        alt="Facebook" 
+        className="w-8 h-8 md:w-10 md:h-10 drop-shadow-lg"
+      />
     </a>
   );
 };
