e9895b5f9fcc95753d037dc511f16a01cb0edf14
Cole Tanner <coletanner02@gmail.com>
2026-09-07 16:12:36 -0700

    chore(SCRUM-444): add board-of-directors url to admin nav bar

--- frontend/config/adminNavLinks.ts
+++ frontend/config/adminNavLinks.ts
@@ -12,4 +12,5 @@ export const adminNavLinks: NavLink[] = [
   { label: 'Permissions Pages', href: '/admin/permissions' },
   { label: 'documentation Pages', href: '/admin/documentation' },
   { label: 'Help Pages', href: '/admin/help' },
+  { label: 'Board of Directors', href: '/admin/board-of-directors'},
 ];
